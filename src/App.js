c3fd2a4a35e0c559a9a998468b23e30c9d35b9a9
helloalexdodd <hello@alexdodd.ca>
2020-05-04 13:44:46 -0400

    refactored secret key

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Header from './components/Header';
 import Weather from './components/Weather';
 import Wrapper from './components/Wrapper';
 import { getData } from './utilities';
-import { WEATHER_API_KEY, GEO_API_KEY } from './constants';
 
 class App extends React.Component {
 	constructor() {
@@ -18,16 +17,6 @@ class App extends React.Component {
 		}
 	};
 
-	getWeather = () => {
-		const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${this.state.lat}&lon=${this.state.lng}&appid=${WEATHER_API_KEY}&units=${this.state.unit}`
-		getData(url).then((weatherData) => this.setState({ weatherData }));
-	};
-
-	getGeo = () => {
-		const url = `http://www.mapquestapi.com/geocoding/v1/address?key=${GEO_API_KEY}&location=${this.state.userInput}}`;
-		getData(url).then((data) => this.saveLocation(data))
-	}
-
 	saveLocation = (data) => {
 		const latLng = data.results[0].locations[0].latLng
 		this.setState({
@@ -36,8 +25,20 @@ class App extends React.Component {
 		}, this.getWeather);
 	};
 
+	getWeather = () => {
+		const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${this.state.lat}&lon=${this.state.lng}&appid=${process.env.REACT_APP_WEATHER_API_KEY}&units=${this.state.unit}`
+		getData(url).then((weatherData) => this.setState({ weatherData }));
+	};
+
+	getGeo = () => {
+		const url = `http://www.mapquestapi.com/geocoding/v1/address?key=${process.env.REACT_APP_GEO_API_KEY}&location=${this.state.userInput}}`;
+		getData(url).then((data) => this.saveLocation(data))
+	}
+
 	handleSubmit = (e) => {
 		e.preventDefault();
+		console.log(process.env.REACT_APP_GEO_API_KEY);
+		console.log(process.env.REACT_APP_WEATHER_API_KEY);
 		this.getGeo();
 	};
 
@@ -61,7 +62,12 @@ class App extends React.Component {
 					handleFocus={this.handleFocus}
 					value={this.state.userInput}
 				/>
-				{this.state.weatherData.daily ? <Weather data={this.state.weatherData} unit={this.state.unit} /> : null}
+				{this.state.weatherData.daily
+					? <Weather
+						data={this.state.weatherData}
+						unit={this.state.unit}
+					/>
+					: null}
 			</Wrapper>
 		);
 	};
